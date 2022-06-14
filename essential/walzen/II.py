729b56ee4a216e4b7db83a249c2fb5f04cc43971
Titanic <93482292+titanic1234@users.noreply.github.com>
2022-06-14 21:45:52 +0200

    Kommentiert

--- essential/walzen/II.py
+++ essential/walzen/II.py
@@ -4,6 +4,8 @@ import os
 import sys
 
 
+#Text geht durch Walze
+
 ab = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 II = ['A', 'J', 'D', 'K', 'S', 'I', 'R', 'U', 'X', 'B', 'L', 'H', 'W', 'T', 'M', 'C', 'Q', 'G', 'Z', 'N', 'P', 'Y', 'F', 'V', 'O', 'E']
